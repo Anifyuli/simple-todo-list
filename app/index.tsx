296faecd5b25d887713ede7999872b6a60a9feae
Anifyuli <anifyuliansyah@gmail.com>
2024-12-18 11:23:06 +0700

    init: finishing with data dummy

---
+++ app/index.tsx
@@ -0,0 +1,127 @@
+import {
+  Text,
+  View,
+  TextInput,
+  Pressable,
+  StyleSheet,
+  FlatList,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
+import { Plus, Trash2 } from "lucide-react-native";
+import data, { Data } from "@/data/todos";
+
+export default function Index() {
+  const [todos, setTodos] = useState(data.sort((a, b) => b.id - a.id));
+  const [text, setText] = useState("");
+
+  const addTodo = () => {
+    if (text.trim()) {
+      const newId = todos.length > 0 ? todos[0].id + 1 : 1;
+      setTodos([{ id: newId, title: text, completed: false }]);
+      setText("");
+    }
+  };
+
+  const toggleTodo = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id == id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const removeTodo = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const renderItem = ({ item }: { item: Data }) => (
+    <View style={style.todoItem}>
+      <Text
+        style={[style.todoText, item.completed && style.completedText]}
+        onPress={() => toggleTodo(item.id)}
+      >
+        {item.title}
+      </Text>
+      <Pressable onPress={() => removeTodo(item.id)}>
+        <Trash2 color="red" />
+      </Pressable>
+    </View>
+  );
+
+  return (
+    <SafeAreaView style={style.container}>
+      <View style={style.inputContainer}>
+        <TextInput
+          style={style.input}
+          placeholder="Add a new todo"
+          placeholderTextColor="gray"
+          value={text}
+          onChangeText={setText}
+        />
+        <Pressable onPress={addTodo} style={style.addButton}>
+          <Plus color="white" />
+        </Pressable>
+      </View>
+      <FlatList
+        data={todos}
+        renderItem={renderItem}
+        keyExtractor={(item) => `${item.id}`}
+        contentContainerStyle={{ flexGrow: 1, marginHorizontal: 10 }}
+      />
+    </SafeAreaView>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+    padding: 10,
+    width: "auto",
+    marginHorizontal: "auto",
+    pointerEvents: "auto",
+  },
+  input: {
+    flex: 1,
+    borderColor: "darkgray",
+    borderWidth: 1,
+    borderRadius: 5,
+    marginRight: 10,
+    fontSize: 18,
+    minWidth: 0,
+    color: "white",
+  },
+  addButton: {
+    backgroundColor: "blue",
+    borderRadius: 5,
+    padding: 10,
+  },
+  todoItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    gap: 4,
+    padding: 10,
+    borderBottomColor: "gray",
+    borderBottomWidth: 1,
+    width: "100%",
+    maxWidth: 1024,
+    marginHorizontal: "auto",
+    pointerEvents: "auto",
+  },
+  todoText: {
+    flex: 1,
+    fontSize: 18,
+    color: "black",
+  },
+  completedText: {
+    textDecorationLine: "line-through",
+    color: "gray",
+  },
+});
